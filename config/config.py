54d9b6c49de354bd4d4f56e678c76b7a3d8c5cde
aloneboy026 <122468535+aloneboy026@users.noreply.github.com>
2023-02-24 20:02:47 +0530

    Update config.py

--- config/config.py
+++ config/config.py
@@ -60,7 +60,7 @@ HEROKU_APP_NAME = getenv("HEROKU_APP_NAME")
 # For customized or modified Repository
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "",
+    "https://github.com/aloneboy026/Missermusic",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "main")
 
